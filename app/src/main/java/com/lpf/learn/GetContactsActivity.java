f6dac5021d16d92ddc4d09c6ee4eab7cea1488f4
liupengfei <pengfei.liu@zentertain.net>
2017-01-18 19:25:26 +0800

    init Commit

---
+++ app/src/main/java/com/lpf/learn/GetContactsActivity.java
@@ -0,0 +1,81 @@
+package com.lpf.learn;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//read contacts from cellphone
+public class GetContactsActivity extends AppCompatActivity {
+    
+    ListView contactsListView;
+    ArrayAdapter<String> contactsAdapter;
+    ArrayList<String> contactsList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_contacts);
+        
+        initViews();
+    }
+
+    private void initViews() {
+        contactsListView = (ListView) findViewById(R.id.contacts);
+        
+        contactsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, contactsList);
+        contactsListView.setAdapter(contactsAdapter);
+        
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_CONTACTS},1);
+        }else{
+            readContacts();
+        }
+    }
+
+    //read contacts
+    private void readContacts() {
+        Cursor cursor = null;
+        try{
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            if(cursor!=null){
+                while(cursor.moveToNext()){
+                    String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contactsList.add(displayName+"\n"+number);
+                }
+                contactsAdapter.notifyDataSetChanged();
+            }
+        }catch (Exception e){
+            
+        }finally {
+            if(cursor!=null){
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    readContacts();
+                }else{
+                    Toast.makeText(this, "just denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}

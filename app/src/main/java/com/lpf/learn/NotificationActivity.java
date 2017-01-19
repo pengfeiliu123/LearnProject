eedc00cc04547fe7540bbfd3765a2ea98bffe45d
liupengfei <pengfei.liu@zentertain.net>
2017-01-19 13:40:13 +0800

    add big text/ big picture for notification

--- app/src/main/java/com/lpf/learn/NotificationActivity.java
+++ app/src/main/java/com/lpf/learn/NotificationActivity.java
@@ -5,7 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,8 +47,17 @@ public class NotificationActivity extends AppCompatActivity {
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                         .setContentIntent(pi)
                         .setAutoCancel(true)                                //show once ,then dismiss
+                        .setVibrate(new long[]{0,1000,1000,1000,1000,1000}) //vibrate one second,slient one second, and loop
+                        .setLights(Color.RED,1000,1000)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText("Learn how to build " +                     //show big text
+                                "notifications,send and sync data,and use voice actions.Get the officaial " +               //show big picture
+                                "Android IDE and developer tools to build apps for Android"))
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
                         .build();
-                notification.defaults |= Notification.DEFAULT_VIBRATE;
+//                notification.defaults |= Notification.DEFAULT_VIBRATE;    //vibrate once
+//                notification.defaults |= Notification.DEFAULT_LIGHTS;
+//                notification.ledARGB = Color.BLUE;
                 manager.notify(notificationId,notification);
             }
         });

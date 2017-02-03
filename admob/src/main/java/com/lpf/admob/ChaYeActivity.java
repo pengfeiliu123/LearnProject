d7c5db2cf8fe70e2c7087b31d5add1a48abc7534
liupengfei <pengfei.liu@zentertain.net>
2017-02-03 19:51:40 +0800

    1.add admob banner/fullscreen demo

---
+++ admob/src/main/java/com/lpf/admob/ChaYeActivity.java
@@ -0,0 +1,64 @@
+package com.lpf.admob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class ChaYeActivity extends AppCompatActivity {
+
+
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cha_ye);
+
+        initFullScreenAd();
+    }
+
+    private void initFullScreenAd() {
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                // to do your thing
+            }
+        });
+
+        requestNewInterstitial();
+
+        findViewById(R.id.btnPlay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mInterstitialAd.isLoaded()){
+                    mInterstitialAd.show();
+                }else{
+                    begin();
+                }
+            }
+        });
+
+        begin();
+
+    }
+
+    private void begin() {
+        Toast.makeText(this, "启动了下一个界面", Toast.LENGTH_SHORT).show();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(adRequest);
+    }
+}

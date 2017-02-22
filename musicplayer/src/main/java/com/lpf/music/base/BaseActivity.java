1a661d01087a3e602927e683af279a95c3ed1a7c
liupengfei <pengfei.liu@zentertain.net>
2017-02-22 20:56:26 +0800

    1.init project

---
+++ musicplayer/src/main/java/com/lpf/music/base/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.lpf.music.base;
+
+import android.support.annotation.Nullable;
+
+import com.afollestad.appthemeengine.ATEActivity;
+
+/**
+ * Created by liupengfei on 17/2/22 20:27.
+ */
+
+public class BaseActivity extends ATEActivity {
+
+    @Nullable
+    @Override
+    public String getATEKey() {
+        return super.getATEKey();
+    }
+}
